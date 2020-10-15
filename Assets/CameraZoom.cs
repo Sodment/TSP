4f7168600f9325328beb193e7bf390a0bfdad3e6
Sodment <sodment@gmail.com>
2020-10-15 11:56:18 +0200

    Well it somehow works

---
+++ Assets/CameraZoom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    private Camera cam;
+    private float targetZoom;
+    private float zoomFactor = 3f;
+    [SerializeField]
+    private float zoomLerpSpeed = 20;
+    void Start()
+    {
+        cam = Camera.main;
+        targetZoom = cam.orthographicSize;
+    }
+    void Update()
+    {
+        float scrollData;
+        scrollData = Input.GetAxis("Mouse ScrollWheel");
+        targetZoom -= scrollData * zoomFactor;
+        targetZoom = Mathf.Clamp(targetZoom, 2.5f, 20f);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime);
+    }
+}
