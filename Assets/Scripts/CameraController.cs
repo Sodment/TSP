b2eae19b97759c165fd2d4df10667ee90d095b2a
Sodment <sodment@gmail.com>
2020-10-15 15:09:32 +0200

    HIGHLITGHTED!

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float targetZoom;
+    public float zoomSpeed;
+    [SerializeField]
+    private float zoomLerpSpeed = 20;
+
+    public float minZoomDist;
+    public float maxZoomDist;
+
+    private Camera cam;
+
+    void Move()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        Vector3 dir = transform.up * yInput + transform.right * xInput;
+        transform.position += dir * moveSpeed * Time.deltaTime;
+    }
+
+    void Zoom()
+    {
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        targetZoom -= scrollInput * zoomSpeed;
+        targetZoom = Mathf.Clamp(targetZoom, minZoomDist, maxZoomDist);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime*zoomLerpSpeed);
+    }
+
+    void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    void Start()
+    {
+        targetZoom = cam.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Zoom();
+    }
+}
---
+++ Assets/Scripts/HighlightMouseOverTile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HighlightMouseOverTile : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Tilemap highligthmap;
+    private Tilemap tileMapComponent;
+    private Tilemap highlightMapComponent;
+    public Tile changer;
+    public Tile highligther;
+    public TileBase prevHigligthed;
+
+    void Start()
+    {
+        tileMapComponent = tilemap.GetComponent<Tilemap>();
+        highlightMapComponent = highligthmap.GetComponent<Tilemap>();
+    }
+    void Update()
+    {
+        if(prevHigligthed != null)
+        {
+
+        }
+
+        Vector3Int mouseOverPosition = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        prevHigligthed = highlightMapComponent.GetTile(mouseOverPosition); 
+        highlightMapComponent.SetTile(mouseOverPosition, highligther);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("Pozycja: " + mouseOverPosition);
+            tileMapComponent.SetTile(mouseOverPosition, changer);
+            
+        }
+    }
+}
+
