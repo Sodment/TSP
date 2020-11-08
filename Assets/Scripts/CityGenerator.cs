95e442a3e8cb7310ca03f618aec325581c0ea497
Sodment <sodment@gmail.com>
2020-11-08 12:20:09 +0100

    CityGenerator

---
+++ Assets/Scripts/CityGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CityGenerator : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Tile cityTile;
+    public int numberOfCities;
+
+    public void GenerateListOfCities()
+    {
+        int i = 0;
+        while(i < numberOfCities)
+        {
+            Vector3Int vector = new Vector3Int
+            {
+                x = Random.Range(-25, 25),
+                y = Random.Range(-24, 25),
+                z = 0
+            };
+            if(tilemap.GetTile(vector) != cityTile)
+            {
+                ListOfCities.instance.CityList.Add(vector);
+                tilemap.SetTile(vector, cityTile);
+                i++;
+            }
+        }
+    }
+}
